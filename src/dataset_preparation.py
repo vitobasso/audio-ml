b50478f2f4f9ab8f9aa38dc84d8206a97a1b6c44
Victor Basso <victorbasso@sagaranatech.com>
2015-02-01 21:26:30 -0300

    dataset loading

--- src/dataset_preparation.py
+++ src/dataset_preparation.py
@@ -2,10 +2,10 @@ __author__ = 'victor'
 
 import os
 import wave
+from scipy.io.wavfile import read
 import contextlib
 
-samplesroot = '../../_dependencies/sound-samples/'
-expected_fmt = dict(rate=44100, swidth=2, ch=1, compr='not compressed')
+import numpy as np
 
 
 def wavinfo(file):
@@ -24,17 +24,46 @@ def validate_fmt(file):
         assert f.getcompname() == expected_fmt['compr'], \
             'compression name is %s. expected %s. file=%s' % (f.getcompname(),  expected_fmt['compr'], file)
 
-def load(dir):
-    print 'loading files...'
+def wavlength(file):
+    with contextlib.closing(wave.open(file,'r')) as f:
+        return f.getnframes()
+
+def __wavread_norm_fmt(x):
+    INT16_FAC = (2**15)-1
+    INT32_FAC = (2**31)-1
+    INT64_FAC = (2**63)-1
+    norm_fact = {'int16':INT16_FAC, 'int32':INT32_FAC, 'int64':INT64_FAC,'float32':1.0,'float64':1.0}
+    return np.float32(x)/norm_fact[x.dtype.name]
+
+def wavread(file, begin, length):
+    #TODO for efficiency, cache the wav in memory or use low level random access w/ the wave module
+    validate_fmt(file)
+    fs, x = read(file)
+    x = __wavread_norm_fmt(x)
+    return x[begin : begin + length]
+
+
+
+samplesroot = '../../_dependencies/sound-samples/'
+expected_fmt = dict(rate=44100, swidth=2, ch=1, compr='not compressed')
+
+def summarize_files(dir):
+    result = []
     loadpath = samplesroot + dir
     for path, dirs, files in os.walk(loadpath):
         for f in files:
-            print f
-            validate_fmt(loadpath + '/' + f)
+            fpath = loadpath + '/' + f
+            validate_fmt(fpath)
+            length = wavlength(fpath)
+            result.append((f, length))
+    return result
 
 
-# wavinfo(root + 'violin/42953__freqman__gypsy-violin-variation.wav')
-# wavinfo('/home/victor/dev/workspace/_dependencies/sms-tools/sounds/carnatic.wav')
-# validate_fmt(root + 'violin/42953__freqman__gypsy-violin-variation.wav')
-
-load('violin')
+snd1 = samplesroot + 'violin/42953__freqman__gypsy-violin-variation.wav'
+snd2 = '/home/victor/dev/workspace/_dependencies/sms-tools/sounds/carnatic.wav'
+# wavinfo(snd1)
+# wavinfo(snd2)
+# validate_fmt(snd1)
+# load('violin')
+# wavread(snd2, 0, 10)
+summarize_files('violin')
