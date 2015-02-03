632c8bec732f2f4d732fea693a9dc7980036f9d8
Victor Basso <victorbasso@sagaranatech.com>
2015-02-03 20:34:39 -0300

    convert.sh

---
+++ sh/convert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# convert all sound files in folder tree to mono 44100 wav
+
+files=$(find . -name "*.wav" -o -name "*.aif" -o -name "*.mp3" -o -name "*.flac")
+for f in $files
+do
+    info="$(soxi -t "$f") $(soxi -c "$f") $(soxi -r "$f")"
+    if [ "$info" != "wav 1 44100" ]; then
+        echo converting $f [$info]
+        fnew=$(echo $f | sed -r s/\\.\\w+/_new.wav/)
+        sox $f -c1 -r44100 $fnew
+        rm $f
+        mv $fnew $f
+    fi
+done
+echo ok
