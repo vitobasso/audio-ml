63abfc78b2fe1f68bf1316caec4403ff98517087
Victor Basso <victorbasso@sagaranatech.com>
2015-02-03 21:42:29 -0300

    convert.sh taking bit sample size into account

--- sh/convert.sh
+++ sh/convert.sh
@@ -5,11 +5,11 @@
 files=$(find . -name "*.wav" -o -name "*.aif" -o -name "*.mp3" -o -name "*.flac")
 for f in $files
 do
-    info="$(soxi -t "$f") $(soxi -c "$f") $(soxi -r "$f")"
-    if [ "$info" != "wav 1 44100" ]; then
+    info="$(soxi -t "$f") $(soxi -c "$f") $(soxi -r "$f") $(soxi -b "$f")"
+    if [ "$info" != "wav 1 44100 16" ]; then
         echo converting $f [$info]
         fnew=$(echo $f | sed -r s/\\.\\w+/_new.wav/)
-        sox $f -c1 -r44100 $fnew
+        sox $f -c1 -r44100 -b16 $fnew
         rm $f
         mv $fnew $f
     fi
